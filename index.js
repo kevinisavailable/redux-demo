1e5b413efd4523fc52a49a98eca3f24a631ffa2b
Aravind Roy <aravindroy1709@gmail.com>
2022-10-29 14:15:41 +0530

    Reducer Updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const CAKE_ORDERED = 'CAKE_ORDERED'  //type property is usually defined as string constants
-
+CAKE_RETURNED = 'CAKE_RETURNED'
 function orderCake(){
     return{
         type : CAKE_ORDERED, //action // something that describes what happened in your application
@@ -8,3 +8,23 @@ function orderCake(){
 }
 
 //Action creator is a function that returns an object
+
+
+const initialState  ={
+    noOfCakes : 10
+}
+
+const Reducer = (state = initialState , action)=>{
+    switch (action.type){
+       case CAKE_ORDERED: 
+            return{
+                noOfCakes : noOfCakes - 1,
+            }
+        case CAKE_RETURNED:
+            return{
+                noOfCakes : noOfCakes + 1
+            }
+        default: 
+        return state
+}
+}
