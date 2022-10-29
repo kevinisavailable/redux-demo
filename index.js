55e7e059dee5d24bf85624435cde7a217c537625
Aravind Roy <aravindroy1709@gmail.com>
2022-10-29 14:04:22 +0530

    Action is updated

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('From Index.js')
+const CAKE_ORDERED = 'CAKE_ORDERED'  //type property is usually defined as string constants
+
+function orderCake(){
+    return{
+        type : CAKE_ORDERED, //action // something that describes what happened in your application
+        quantity : 1,   //state
+}
+}
+
+//Action creator is a function that returns an object
